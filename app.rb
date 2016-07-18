8d064d876ce7b8de796acc449903f8e944274de1
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-18 18:28:53 +0100

    Sinatra: Returning HTML

--- app.rb
+++ app.rb
@@ -8,10 +8,8 @@ get '/secret' do
   "Hello from Zee and Ty!!"
 end
 
-get "/route2" do
-  "hello from route2"
-end
-
-get "/route3" do
-  "hello from route3"
+get '/cat' do
+    "<div style='border: 3px dotted green' >
+    <img src='http://bit.ly/1eze8aE'>
+    </div>"
 end
