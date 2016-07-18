44d8e993e373e6c7116ccb278c99c68487873037
Titus Iudean <iudeant@gmail.com>
2016-07-18 19:59:22 +0300

    Creates a few more routes and uses Shotgun for Sinatra

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello world"
 end
+
+get '/secret' do
+  "Hello from Zee and Ty!!"
+end
+
+get "/route2" do
+  "hello from route2"
+end
+
+get "/route3" do
+  "hello from route3"
+end
